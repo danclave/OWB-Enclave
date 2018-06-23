f15562714c5cacd248950628c8c68b6df1fd5611
unknown <jef_wauters@telenet.be>
2018-06-23 14:47:52 +0200

    massive commit of Reformer work, lots of images and audio added

---
+++ interface/generateEventIcons.ps1
@@ -0,0 +1,26 @@
+cd 'C:\Users\jef\Documents\Paradox Interactive\Hearts of Iron IV\mod\enclaverb\gfx'
+$names = ls -name -Path event_pictures;
+
+$names = $names.replace(".tga","")
+
+
+$resultBody = Foreach ($name in $names)
+
+{
+    "
+        SpriteType = {#" + $name + "
+            name = `"GFX_" + $name + "`"
+ 		    texturefile = `"gfx/event_pictures/"+$name+".tga`"
+ 	    }"
+};
+
+"spriteTypes = {" + $resultBody + $resultIdeas +  "    
+}" | Out-File -FilePath ..\..\interface/enc_eventpictures.gfx -Encoding utf8
+
+$events_file = Foreach ($name in $names)
+{
+   "# GFX_$name"
+};
+ "events = {" + $events_file + "    
+}" 
+$events_file | Out-File -FilePath ..\..\events_file.txt -Encoding utf8 ;
